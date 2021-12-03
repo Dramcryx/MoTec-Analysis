d380a8e99784fb87e49de39af984ab5b47f44f22
Dramcryx <jopa19991@gmail.com>
2021-12-03 12:54:03 +0300

    Remove older files, fix scripts

--- execpipeline.sh
+++ execpipeline.sh
@@ -5,16 +5,20 @@ cutby=$2
 hours=$3
 preprocessdir=$4
 
-rf -rf $preprocessdir
+if [ -d $preprocessdir ]
+then
+    rm -rf $preprocessdir/
+fi
 
 mkdir $preprocessdir
 
 echo "args - $@"
 
-python ./main2.py --cut=$cutby --hours=$hours --file=$input --output=$preprocessdir
+./preprocessor --cut=$cutby --hours=$hours --file=$input --output=$preprocessdir
 
 ~/.local/bin/spark-submit --master=local[2] ./spark.py --file=$preprocessdir/outdata.csv --output=$preprocessdir --hours=$hours --cut=$cutby
 
 mv $(find $preprocessdir/spark/ -iname "*.csv") $preprocessdir/outdata_final.csv
+rm -rf $preprocessdir/spark/
 
-python ./clickhouse-loader.py --file=$preprocessdir
+./clickhouse-loader --file=$preprocessdir
