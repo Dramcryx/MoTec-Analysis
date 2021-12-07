adcd90a96196cd65101c2428b8b95b03fef65d35
Dramcryx <jopa19991@gmail.com>
2021-12-07 23:39:33 +0300

    Make command strictly piped

--- luckyone.sh
+++ luckyone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./install; cd dist; docker-compose up & sleep 0; sleep 10; cd ../; ./initgrafana;
+./install && cd dist && (docker-compose up & sleep 0) && sleep 25 && cd ../ && ./initgrafana && cd dist && docker-compose down && cd ../ && echo "DONE!"
